37dbc927c512d13594187cacf19f25d5fcd9904a
Peter Fronc <peter.fronc@qubitdigital.com>
2014-05-08 16:03:04 +0100

    Cleaning up shared.

--- shared/bin/build.sh
+++ shared/bin/build.sh
@@ -30,68 +30,11 @@ echo "==========================================================================
 echo "Cleaning: Removing dist"
 echo
 
-rm -rf dist
+rm -rf $SRC/dist
 mkdir $SRC/dist
 
 echo "Build: Generating full package (with dependencies): dist/out-all-src.js"
 
 #build output with all dependencies
-java -jar $TOOLS/bin/LibraryWizard.jar  --build $SRC
+java -jar $TOOLS/bin/LibraryWizard.jar  --build-and-run $SRC
 
-#java -jar $TOOLS/bin/MiniMerge.jar \
-# -o $SRC/dist/out-all-src.js \
-# -i .js \
-# --source-base "$SRC_BASE"\
-# -s $SRC\
-# --info\
-# --exclude-file-path-patterns "$EXCLUDE"\
-# -v
-#
-#
-#echo "================================================================================"
-#echo "Build: Generating library: dist/$TAG-release.js"
-#echo
-#
-##build output with this sources ONLY 
-##(it will complain for missing files - its normal)
-#java -jar $MM \
-# -o $SRC/dist/$TAG-release.js \
-# -i .js \
-# --source-base "$SRC_BASE"\
-# -s $SRC\
-# --info\
-# --exclude-file-path-patterns "$EXCLUDE"\
-# -v
-#
-#echo "================================================================================"
-#echo "Build: Generating debug pages..."
-#echo
-#
-##build index for debugging.
-#java -jar $MM \
-# -o $HTML/scripts.htmlf \
-# -i .js \
-# --source-base "$SRC_BASE"\
-# --exclude-file-path-patterns "$EXCLUDE"\
-# -s $SRC\
-# --info\
-# --index\
-# --prefix "<script src='../../../"\
-# --suffix "'></script>"\
-# --add-base\
-# -vv
-
-##prepare debug page
-#cat $HTML/prefix.html > $HTML/index.html
-#cat $HTML/scripts.htmlf >> $HTML/index.html
-#cat $HTML/body.html >> $HTML/index.html
-#cat $HTML/suffix.html >> $HTML/index.html
-
-
-#clean up
-rm $HTML/scripts.htmlf
-
-
-#echo "Done. Open index.html file in a browser."
-
-#open index.html
