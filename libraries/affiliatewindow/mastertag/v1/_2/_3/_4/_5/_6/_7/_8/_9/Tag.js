25901fa784922364f4c615415a956d35ea716737
Peter Fronc <peter.fronc@qubitdigital.com>
2014-05-29 15:11:32 +0100

    Adding default v1 for wizard.

---
+++ libraries/affiliatewindow/mastertag/v1/_2/_3/_4/_5/_6/_7/_8/_9/Tag.js
@@ -0,0 +1,36 @@
+//:include tagsdk-current.js
+
+qubit.opentag.LibraryTag.define("affiliatewindow.mastertag.v1._2._3._4._5._6._7._8._9.Tag", {
+	config: {
+		/*DATA*/
+		name: "Master Tag",
+		async: true,
+		description: "The Affiliate Window Master Tag should run on all pages.",
+		html: "",
+		imageUrl: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "www.dwin1.com/${MERCHANT_ID}.js",
+		usesDocWrite: true,
+		upgradeable: true,
+		parameters: [{
+			name: "Affiliate Window Merchant ID",
+			description: "Affiliate Window Merchant ID",
+			token: "MERCHANT_ID",
+			uv: ""
+		}]
+		/*~DATA*/
+	},
+	script: function() {
+		/*SCRIPT*/
+		/*~SCRIPT*/
+	},
+	pre: function() {
+		/*PRE*/
+		/*~PRE*/
+	},
+	post: function() {
+		/*POST*/
+		/*~POST*/
+	}
+});
