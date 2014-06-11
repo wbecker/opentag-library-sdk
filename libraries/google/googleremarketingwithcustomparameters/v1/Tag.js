b85e2263defe4a1695aa91403847e1cf26da8ac9
Peter Fronc <peter.fronc@qubitdigital.com>
2014-06-11 13:28:02 +0100

    Update. Google.

--- libraries/google/googleremarketingwithcustomparameters/v1/Tag.js
+++ libraries/google/googleremarketingwithcustomparameters/v1/Tag.js
@@ -6,7 +6,7 @@ qubit.opentag.LibraryTag.define(
 			/*DATA*/
 			name: "Google Remarketing with custom parameters",
 			async: true,
-			description: "The standard GA re-marketing tag, but allows for custom parameter as per the guide: http://bit.ly/14iZMqu. Return an object within an anonymous function to populate the parameters.",
+			description: "",
 			html: "",
 			locationDetail: "",
 			isPrivate: false,
@@ -20,28 +20,18 @@ qubit.opentag.LibraryTag.define(
 				uv: ""
 			}, {
 				name: "Product ID",
-				description: "On a product page input the uv.product.id, on a transaction/listing, input an array of ids.",
+				description: "Product ID",
 				token: "product_id",
 				uv: ""
 			}, {
 				name: "Page Type",
-				description: "Valid values: home, searchresults, category, product, basket, purchase, other",
+				description: "Page Type",
 				token: "page_type",
 				uv: ""
 			}, {
-				name: "Total value/price",
-				description: "Specify the value of the product. On a basket/purchase page, this should be the total sum of items.",
+				name: "Order Value",
+				description: "Order Value",
 				token: "total",
-				uv: ""
-			}, {
-				name: "Google Conversion ID",
-				description: "Your Google id provided in the script",
-				token: "conversion_id",
-				uv: ""
-			}, {
-				name: "Google Conversion Label",
-				description: "A alphanumeric label of your conversion tracking",
-				token: "label",
 				uv: ""
 			}]
 			/*~DATA*/
@@ -56,17 +46,24 @@ qubit.opentag.LibraryTag.define(
 		},
 		post: function() {
 			/*POST*/
-			window.google_trackConversion({
-				google_conversion_id: "" + this.valueForToken("conversion_id"),
-				google_conversion_label: "" + this.valueForToken("label"),
-				google_custom_params: {
-					ecomm_prodid: this.valueForToken("product_id"),
-					ecomm_pagetype: "" + this.valueForToken("page_type"),
-					ecomm_totalvalue: this.valueForToken("total")
-				},
-				google_remarketing_only: true
-			});
+			var _this = this;
+			var poll = function() {
+				if (window.google_trackConversion) {
+					window.google_trackConversion({
+						google_conversion_id: "" + _this.valueForToken("google_id"),
+						google_custom_params: {
+							ecomm_prodid: "" + _this.valueForToken("product_id"),
+							ecomm_pagetype: "" + _this.valueForToken("page_type"),
+							ecomm_totalvalue: _this.valueForToken("total")
+						},
+						google_remarketing_only: true
+					});
+				} else {
+					setTimeout(poll, 100);
+				}
+			};
 
+			poll();
 			/*~POST*/
 		}
 	});
