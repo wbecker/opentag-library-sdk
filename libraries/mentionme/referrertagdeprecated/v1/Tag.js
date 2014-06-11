6aaf98d573ba71e3dec7d6c4987ffcbdd8ffa03f
Peter Fronc <peter.fronc@qubitdigital.com>
2014-06-11 13:51:23 +0100

    Validation.

--- libraries/mentionme/referrertagdeprecated/v1/Tag.js
+++ libraries/mentionme/referrertagdeprecated/v1/Tag.js
@@ -3,9 +3,9 @@
 qubit.opentag.LibraryTag.define("mentionme.referrertagdeprecated.v1.Tag", {
 	config: {
 		/*DATA*/
-		name: "Referrer Tag [Deprecated]",
+		name: "Referrer Tag [DEPRECATED]",
 		async: true,
-		description: "The implementation can be either as a modal popup or embedded in the page using an iFrame, both of which lead to a popup where the customer can register to become a referrer. If the client requires, they can set the implementation parameter to 'embed', in which case the content is loaded into an iframe within the page. This is inserted within a DIV <div id=\"mmWrapper\"></div> which should be on the page.\n\nAll parameters marked with * are optional (if not used populate with empty hardcoded value, even if default is 'uses universal variable')",
+		description: "The implementation can be either as a modal popup or embedded in the page using an iFrame, both of which lead to a popup where the customer can register to become a referrer. If the client requires, they can set the implementation parameter to 'embed', in which case the content is loaded into an iframe within the page. This is inserted within a DIV <div id=\"mmWrapper\"></div> which should be on the page. All parameters marked with * are optional (if not used populate with empty hardcoded value, even if default is 'uses universal variable')",
 		html: "",
 		locationDetail: "",
 		isPrivate: true,
@@ -176,7 +176,6 @@ qubit.opentag.LibraryTag.define("mentionme.referrertagdeprecated.v1.Tag", {
 		function beginningZero(digit) {
 			return (digit < 10) ? "0" + digit : "" + digit;
 		}
-
 		/*~SCRIPT*/
 	},
 	pre: function() {
