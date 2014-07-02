1767871ba05942a62a1c96fc6b2a37552544955e
Peter Fronc <peter.fronc@qubitdigital.com>
2014-07-02 13:04:02 +0100

    Updates.

--- libraries/marinsoftware/zzclicktrackingtagdeprecated/v1/Tag.js
+++ libraries/marinsoftware/zzclicktrackingtagdeprecated/v1/Tag.js
@@ -6,7 +6,7 @@ qubit.opentag.LibraryTag.define(
 			/*DATA*/
 			name: "zz-Click Tracking Tag [DEPRECATED]",
 			async: true,
-			description: "The Marin Click Tracking Tag helps Marin to capture all sources of website traffic, and set a 1st party cookie enabling it to apply deeper insights to measure their return on investment for media managed in the Marin Enterprise platform. The Marin Click Tracking Tag is to be exposed on all landing pages of the website, ideally by being placed in a global template such as a footer template.",
+			description: "",
 			html: "",
 			locationDetail: "",
 			isPrivate: true,
@@ -14,14 +14,9 @@ qubit.opentag.LibraryTag.define(
 			usesDocWrite: false,
 			upgradeable: true,
 			parameters: [{
-				name: "Anonymize User IP",
-				description: "\"yes\" or \"no\"",
-				token: "anonymize_ip",
-				uv: ""
-			}, {
 				name: "Marin Tracking ID",
-				description: "Client Specific Marin Tracking ID",
-				token: "marin_tracking_id",
+				description: "",
+				token: "id",
 				uv: ""
 			}]
 			/*~DATA*/
@@ -29,16 +24,11 @@ qubit.opentag.LibraryTag.define(
 		script: function() {
 			/*SCRIPT*/
 			window._mTrack = window._mTrack || [];
-
-			if (/^\s*yes\s*$/i.test("" + this.valueForToken("anonymize_ip"))) {
-				_mTrack.push(['activateAnonymizeIp']);
-			}
-
 			_mTrack.push(['trackPage']);
 
-			var mClientId = "" + this.valueForToken("marin_tracking_id");
-			var mProto = ('https:' == document.location.protocol ? 
-			'https://' : 'http://');
+			var mClientId = "" + this.valueForToken("id") + "";
+			var mProto = ('https:' == document.location.protocol ? 'https://' :
+				'http://');
 			var mHost = 'tracker.marinsm.com';
 			var mt = document.createElement('script');
 			mt.type = 'text/javascript';
@@ -46,8 +36,6 @@ qubit.opentag.LibraryTag.define(
 			mt.src = mProto + mHost + '/tracker/async/' + mClientId + '.js';
 			var fscr = document.getElementsByTagName('script')[0];
 			fscr.parentNode.insertBefore(mt, fscr);
-
-
 			/*~SCRIPT*/
 		},
 		pre: function() {
