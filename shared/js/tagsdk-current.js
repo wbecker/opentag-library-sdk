6e20d8e0ff9d3eef75e632e33202f6358e71202e
Peter Fronc <peter.fronc@qubitdigital.com>
2014-07-07 12:00:24 +0100

    Clean updates.

--- shared/js/tagsdk-current.js
+++ shared/js/tagsdk-current.js
@@ -3136,222 +3136,6 @@ q.html.HtmlInjector.getAttributes = function (node) {
 
 
 
-(function(){
-  var Utils = qubit.opentag.Utils;
-  var log = new qubit.opentag.Log("Tags -> ");
-  
-  /**
-   * @singleton
-   * @class qubit.opentag.Tags
-   * #Tags Utility Class
-   * This class is an utlity object for accessing, grouping and filtering tags.
-   */
-  var Tags = function () {};
-
-  Utils.clazz("qubit.opentag.Tags", Tags);
-
-  /**
-   * Returns all tags grouped by logical state, it collects ALL tags from ALL
-   * Containers - tags unregistered will not be included. 
-   * @returns {Object}
-   */
-  Tags.getAllTagsByState = function () {
-    return qubit.opentag.Container.getAllTagsByState(Tags.getTags());
-  };
-  
-  /**
-   * Find tag by name
-   * @param {String} match string, String.match() function will be used.
-   * @returns {Array} array of qubit.opentag.BaseTag
-   */
-  Tags.findTagByName = function (match) {
-    var tags = this.getTags();
-    var results = [];
-    for(var i = 0; i < tags.length; i++) {
-      if (tags[i].config.name === match) {
-        results.push(tags[i]);
-      }
-    }
-    return results;
-  };
-  
-  /**
-   * Find tag by name
-   * @param {String} match string, String.match() function will be used.
-   * @returns {Array} array of qubit.opentag.BaseTag
-   */
-  Tags.findTagByMatch = function (match) {
-    var tags = this.getTags();
-    var results = [];
-    for(var i = 0; i < tags.length; i++) {
-      if (tags[i].config.name.match(match)) {
-        results.push(tags[i]);
-      }
-    }
-    return results;
-  };
-  /**
-   * Get all opentag instances map.
-   * 
-   * @returns Object
-   */
-  Tags.getTags = function () {
-    log.FINEST("getTags");
-    return qubit.opentag.BaseTag.getTags();
-  };
-  
-  /**
-   * Reset all tags. It will make all tags ready to rerun like they never
-   * were run.
-   * 
-   * @returns Object
-   */
-  Tags.resetAllTags = function () {
-    log.WARN("reseting all tags!");
-    var tags = Tags.getTags();
-    for (var i = 0; i < tags.length; i++) {
-      tags[i].reset();
-    }
-  };
-
-  /**
-   * Returns all page variable instances associated with containers.
-   * @returns {Array}
-   */
-  Tags.getContainersPageVariables = function () {
-    var containers = Tags.getContainers();
-    var vars = [];
-    for(var i =0; i < containers.length; i++) {
-      vars = vars.concat(containers.getPageVariables());
-    }
-    return vars;
-  };
-  
-  /**
-   * @static
-   * Returns all page variables associated with all registered tags.
-   */
-  Tags.getAllPageVariables = function () {
-    var tags = Tags.getTags();
-    var vars = [];
-    for(var i = 0; i < tags.length; i++) {
-      vars = vars.concat(tags[i].getPageVariables());
-    }
-    return vars;
-  };
-
-  /**
-   * @static
-   * Cancell all tags.
-   */
-  Tags.cancelAll = function () {
-    var tags = Tags.getTags();
-    for(var i = 0; i < tags.length; i++) {
-      tags[i].cancel();
-    }
-  };
-  
-  /**
-   * @static
-   * Reset all tags.
-   */
-  Tags.resetAll = function () {
-    var tags = Tags.getTags();
-    for(var i = 0; i < tags.length; i++) {
-      tags[i].reset();
-    }
-  };
-
-  /**
-   * Function used to get all page variables instances having same name.
-   * 
-   * @param {String} name token name that identifies the variable.
-   * @return {qubit.opentag.pagevariable.BaseVariable} object 
-       qubit.opentag.pagevariable.BaseVariable
-       instance. 
-   */
-  Tags.getPageVariableByName = function (name) {
-    var vars = Tags.getAllPageVariables();
-    var rets = [];
-    for (var i = 0; i < vars.length; i++) {
-      if (vars[i].config.name === name) {
-        rets.push(vars[i]);
-      }
-    }
-    return rets;
-  };
-  
-  /**
-   * Gets load time for tag.
-   * 
-   * @param {qubit.opentag.BaseTag} tag
-   * @returns {Object} Object return has two properties:
-   * 
-   * - `tag` reference to tag
-   * 
-   * - `loadTime` Number value or null if tag is finished running (note, it does
-   *  not check if tag is loaded successfuly but if it was run)
-   */
-  Tags.getLoadTime = function (tag) {
-    var start = tag.beforeRun;
-    var end = tag.runIsFinished;
-    if (isNaN(end)) {
-      return {tag: tag, loadTime: null};
-    } else {
-      return {tag: tag, loadTime: (end - start)};
-    }
-  };
-
-  /**
-   * Gets load times for all tags or passed via argument.
-   * 
-   * @param {Object} tags tags map or an array (optional)
-   */
-  Tags.getLoadTimes = function (tags) {
-    var ret = [];
-    if (tags instanceof qubit.opentag.BaseTag) {
-      ret.push([Tags.getLoadTime(tags[prop])]);
-      return ret;
-    }
-    
-    tags = tags || Tags.getTags();
-    
-    var array = tags instanceof Array;
-    
-    if (array) {
-      for(var i = 0; i < tags.length; i++) {
-        if (tags[i] instanceof qubit.opentag.BaseTag) {
-          ret.push(Tags.getLoadTime(tags[i]));
-        }
-      }
-    } else {
-      for(var prop in tags) {
-        if (tags[prop] instanceof qubit.opentag.BaseTag) {
-          ret.push(Tags.getLoadTime(tags[prop]));
-        }
-      }
-    }
-    return ret;
-  };
-    
-  
-  /**
-   * Containers getter.
-   * Gets all containers registered.
-   * @returns {Array} array of qubit.opentag.Container
-   */
-  Tags.getContainers = function () {
-    return qubit.opentag.Container.getContainers();
-  };
-  
-  log.INFO("*** Qubit TagSDK *** ", true,
-           "font-size: 22px; color:#CCC;"+//L
-           "text-shadow:#fff 0px 1px 0, #555 0 -1px 0;");//L
-})();
-
-
-
-
 /*
  * Opentag, a tag deployment platform
  * Copyright 2013-2014, Qubit Group
@@ -3682,8 +3466,6 @@ q.html.simplecookie.writeCookie = function (name, value, days, domain) {
 
 
 
-
-
 (function () {
   var Utils = qubit.opentag.Utils;
   var TagsUtils = qubit.opentag.TagsUtils;
@@ -3941,8 +3723,6 @@ q.html.simplecookie.writeCookie = function (name, value, days, domain) {
     }
   };
 }());
-
-
 
 
 
@@ -6534,6 +6314,222 @@ q.html.simplecookie.writeCookie = function (name, value, days, domain) {
 }());
 
 
+
+
+(function(){
+  var Utils = qubit.opentag.Utils;
+  var log = new qubit.opentag.Log("Tags -> ");
+  
+  /**
+   * @singleton
+   * @class qubit.opentag.Tags
+   * #Tags Utility Class
+   * This class is an utlity object for accessing, grouping and filtering tags.
+   */
+  var Tags = function () {};
+
+  Utils.clazz("qubit.opentag.Tags", Tags);
+
+  /**
+   * Returns all tags grouped by logical state, it collects ALL tags from ALL
+   * Containers - tags unregistered will not be included. 
+   * @returns {Object}
+   */
+  Tags.getAllTagsByState = function () {
+    return qubit.opentag.Container.getAllTagsByState(Tags.getTags());
+  };
+  
+  /**
+   * Find tag by name
+   * @param {String} match string, String.match() function will be used.
+   * @returns {Array} array of qubit.opentag.BaseTag
+   */
+  Tags.findTagByName = function (match) {
+    var tags = this.getTags();
+    var results = [];
+    for(var i = 0; i < tags.length; i++) {
+      if (tags[i].config.name === match) {
+        results.push(tags[i]);
+      }
+    }
+    return results;
+  };
+  
+  /**
+   * Find tag by name
+   * @param {String} match string, String.match() function will be used.
+   * @returns {Array} array of qubit.opentag.BaseTag
+   */
+  Tags.findTagByMatch = function (match) {
+    var tags = this.getTags();
+    var results = [];
+    for(var i = 0; i < tags.length; i++) {
+      if (tags[i].config.name.match(match)) {
+        results.push(tags[i]);
+      }
+    }
+    return results;
+  };
+  /**
+   * Get all opentag instances map.
+   * 
+   * @returns Object
+   */
+  Tags.getTags = function () {
+    log.FINEST("getTags");
+    return qubit.opentag.BaseTag.getTags();
+  };
+  
+  /**
+   * Reset all tags. It will make all tags ready to rerun like they never
+   * were run.
+   * 
+   * @returns Object
+   */
+  Tags.resetAllTags = function () {
+    log.WARN("reseting all tags!");
+    var tags = Tags.getTags();
+    for (var i = 0; i < tags.length; i++) {
+      tags[i].reset();
+    }
+  };
+
+  /**
+   * Returns all page variable instances associated with containers.
+   * @returns {Array}
+   */
+  Tags.getContainersPageVariables = function () {
+    var containers = Tags.getContainers();
+    var vars = [];
+    for(var i =0; i < containers.length; i++) {
+      vars = vars.concat(containers.getPageVariables());
+    }
+    return vars;
+  };
+  
+  /**
+   * @static
+   * Returns all page variables associated with all registered tags.
+   */
+  Tags.getAllPageVariables = function () {
+    var tags = Tags.getTags();
+    var vars = [];
+    for(var i = 0; i < tags.length; i++) {
+      vars = vars.concat(tags[i].getPageVariables());
+    }
+    return vars;
+  };
+
+  /**
+   * @static
+   * Cancell all tags.
+   */
+  Tags.cancelAll = function () {
+    var tags = Tags.getTags();
+    for(var i = 0; i < tags.length; i++) {
+      tags[i].cancel();
+    }
+  };
+  
+  /**
+   * @static
+   * Reset all tags.
+   */
+  Tags.resetAll = function () {
+    var tags = Tags.getTags();
+    for(var i = 0; i < tags.length; i++) {
+      tags[i].reset();
+    }
+  };
+
+  /**
+   * Function used to get all page variables instances having same name.
+   * 
+   * @param {String} name token name that identifies the variable.
+   * @return {qubit.opentag.pagevariable.BaseVariable} object 
+       qubit.opentag.pagevariable.BaseVariable
+       instance. 
+   */
+  Tags.getPageVariableByName = function (name) {
+    var vars = Tags.getAllPageVariables();
+    var rets = [];
+    for (var i = 0; i < vars.length; i++) {
+      if (vars[i].config.name === name) {
+        rets.push(vars[i]);
+      }
+    }
+    return rets;
+  };
+  
+  /**
+   * Gets load time for tag.
+   * 
+   * @param {qubit.opentag.BaseTag} tag
+   * @returns {Object} Object return has two properties:
+   * 
+   * - `tag` reference to tag
+   * 
+   * - `loadTime` Number value or null if tag is finished running (note, it does
+   *  not check if tag is loaded successfuly but if it was run)
+   */
+  Tags.getLoadTime = function (tag) {
+    var start = tag.beforeRun;
+    var end = tag.runIsFinished;
+    if (isNaN(end)) {
+      return {tag: tag, loadTime: null};
+    } else {
+      return {tag: tag, loadTime: (end - start)};
+    }
+  };
+
+  /**
+   * Gets load times for all tags or passed via argument.
+   * 
+   * @param {Object} tags tags map or an array (optional)
+   */
+  Tags.getLoadTimes = function (tags) {
+    var ret = [];
+    if (tags instanceof qubit.opentag.BaseTag) {
+      ret.push([Tags.getLoadTime(tags[prop])]);
+      return ret;
+    }
+    
+    tags = tags || Tags.getTags();
+    
+    var array = tags instanceof Array;
+    
+    if (array) {
+      for(var i = 0; i < tags.length; i++) {
+        if (tags[i] instanceof qubit.opentag.BaseTag) {
+          ret.push(Tags.getLoadTime(tags[i]));
+        }
+      }
+    } else {
+      for(var prop in tags) {
+        if (tags[prop] instanceof qubit.opentag.BaseTag) {
+          ret.push(Tags.getLoadTime(tags[prop]));
+        }
+      }
+    }
+    return ret;
+  };
+    
+  
+  /**
+   * Containers getter.
+   * Gets all containers registered.
+   * @returns {Array} array of qubit.opentag.Container
+   */
+  Tags.getContainers = function () {
+    return qubit.opentag.Container.getContainers();
+  };
+  
+  log.INFO("*** Qubit TagSDK *** ", true,
+           "font-size: 22px; color:#CCC;"+//L
+           "text-shadow:#fff 0px 1px 0, #555 0 -1px 0;");//L
+})();
+
+
 q.cookie = {};
 
 q.cookie.PageView = {};
@@ -7549,7 +7545,6 @@ var JSON = {};
 
 
 
-
 /*
  * Opentag, a tag deployment platform
  * Copyright 2013-2014, Qubit Group
@@ -7559,7 +7554,6 @@ var JSON = {};
 
 (function(){
   var Utils = qubit.opentag.Utils;
-  //var BaseVariable = qubit.opentag.pagevariable.BaseVariable;
   var BaseFilter = qubit.opentag.filter.BaseFilter;
   var BaseTag = qubit.opentag.BaseTag;
   var Timed = qubit.opentag.Timed;
