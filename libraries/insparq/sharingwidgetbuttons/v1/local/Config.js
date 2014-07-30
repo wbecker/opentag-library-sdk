2fcbed1bbc2e0b735360d4514b873dd7b0f83af5
Alex Olivier <alex@alexolivier.me>
2014-07-30 17:01:52 +0100

    added insparq requested changes and removed evals

---
+++ libraries/insparq/sharingwidgetbuttons/v1/local/Config.js
@@ -0,0 +1,35 @@
+/**ignore at merge**/
+//:include tagsdk-current.js
+qubit.opentag.Utils.namespace('insparq.sharingwidgetbuttons.v1.local');
+insparq.sharingwidgetbuttons.v1.local.Config = {
+  "parameters": [
+     {
+      "name": "Client API Key",
+      "description": "Client API Key",
+      "token": "key",
+      "uv": "",
+      "inputVariable": "\"123\""
+    },
+     {
+      "name": "jQuery Element Selector",
+      "description": "e.g. insertAfter(...)  ,  appendTo(...)  , insertBefore(....) etc.",
+      "token": "selector",
+      "uv": "",
+      "inputVariable": "\"#content\""
+    },
+     {
+      "name": "jQuery",
+      "description": "e,g, jQuery , $ , myJquery etc.",
+      "token": "jQuery",
+      "uv": "",
+      "inputVariable": "\"jQuery\""
+    },
+     {
+      "name": "API Domain",
+      "description": "The domain to use for the API server",
+      "token": "apidomain",
+      "uv": "",
+      "inputVariable": "\"api\""
+    }
+  ]
+};
---
+++ libraries/insparq/trendingproductsfeed/v1/local/Config.js
@@ -0,0 +1,42 @@
+/**ignore at merge**/
+//:include tagsdk-current.js
+qubit.opentag.Utils.namespace('insparq.trendingproductsfeed.v1.local');
+insparq.trendingproductsfeed.v1.local.Config = {
+  "parameters": [
+     {
+      "name": "jQuery",
+      "description": "e.g. window.jQuery , $ , myJquery , etc..",
+      "token": "jQuery",
+      "uv": "",
+      "inputVariable": "\"jQuery\""
+    },
+     {
+      "name": "jQuery Element Selector to Insert After",
+      "description": "e.g. #content",
+      "token": "selector",
+      "uv": "",
+      "inputVariable": "\"#content\""
+    },
+     {
+      "name": "Client API Key",
+      "description": "Client API Key",
+      "token": "client",
+      "uv": "",
+      "inputVariable": "\"test\""
+    },
+     {
+      "name": "URL",
+      "description": "url",
+      "token": "url",
+      "uv": "",
+      "inputVariable": "\"test\""
+    },
+     {
+      "name": "Pinboard Domain",
+      "description": "The Pinboard domain to use.",
+      "token": "pinboard_domain",
+      "uv": "",
+      "inputVariable": "\"pin\""
+    }
+  ]
+};
