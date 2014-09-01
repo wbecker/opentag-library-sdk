52ab4efb505d59eedef77ac6e2b24d037d1bb3be
Craig Bojko <craig@qubitproducts.com>
2014-09-01 21:35:34 +0100

    Implemented TVSquared tag

---
+++ libraries/tvsquared/action/v1/local/Config.js
@@ -0,0 +1,63 @@
+/**ignore at merge**/
+//:include tagsdk-current.js
+qubit.opentag.Utils.namespace('tvsquared.action.v1.local');
+tvsquared.action.v1.local.Config = {
+  "parameters": [
+     {
+      "name": "TVSquared Client ID",
+      "description": "Will be supplied by TVSquared, and specifies the clients individual ID",
+      "token": "clientID",
+      "uv": "",
+      "inputVariable": "\"123\""
+    },
+     {
+      "name": "TVSquared Brand ID",
+      "description": "Will be supplied by TVSquared, and specifies the brand within the client (separate countries, websites etc.)",
+      "token": "brandID",
+      "uv": "",
+      "inputVariable": "\"123\""
+    },
+     {
+      "name": "User ID",
+      "description": "Client identifiable reference of the user taking the action.",
+      "token": "userID",
+      "uv": "universal_variable.user.user_id",
+      "inputVariable": "\"1\""
+    },
+     {
+      "name": "Action ID",
+      "description": "If the client has a specific ID for the action, add here.",
+      "token": "actionID",
+      "uv": "",
+      "inputVariable": "\"123\""
+    },
+     {
+      "name": "Action Name",
+      "description": "The name of the action captured (Conversion, Quote etc.).",
+      "token": "actionName",
+      "uv": "",
+      "inputVariable": "\"test\""
+    },
+     {
+      "name": "Revenue",
+      "description": "For a sale, the total sales value.",
+      "token": "revenue",
+      "uv": "",
+      "inputVariable": "\"10.00\""
+    },
+     {
+      "name": "Product",
+      "description": "Can be used to indicate an overall product type the action refers to (e.g. a home insurance quote, or a recurring conversion).",
+      "token": "product",
+      "uv": "",
+      "inputVariable": "\"abc\""
+    },
+     {
+      "name": "Promotion Code",
+      "description": "If the client uses Promo codes to attribute data, the code can be added here.",
+      "token": "promoCode",
+      "uv": "",
+      "inputVariable": "\"abc123\""
+    }
+  ]
+};
---
+++ libraries/tvsquared/tracking/v1/Tag.js
@@ -0,0 +1,48 @@
+//:include tagsdk-current.js
+
+qubit.opentag.LibraryTag.define("tvsquared.tracking.v1.Tag", {
+	config: {
+		/*DATA*/
+		name: "Tracking Tag",
+		async: true,
+		description: "The basic hit tracker should be fired on all pages of the site. Our service is territory specific, so clients should be able to setup rules to ensure that the tag only fires on the US (or UK, or French) site, if their website is global. ",
+		html: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "",
+		usesDocWrite: false,
+		upgradeable: true,
+		parameters: [{
+			name: "TVSquared Client ID",
+			description: "Will be supplied by TVSquared, and specifies the clients individual ID",
+			token: "clientID",
+			uv: ""
+		}, {
+			name: "TVSquared Brand ID",
+			description: "Will be supplied by TVSquared, and specifies the brand within the client (separate countries, websites etc.)",
+			token: "brandID",
+			uv: ""
+		}]
+		/*~DATA*/
+	},
+	script: function() {
+		var _paq = _paq || [];
+		var that = this;
+    	(function () {
+	    	var u = (("https:" == document.location.protocol) ? "https://collector-"+that.valueForToken('clientID')+".tvsquared.com/piwik/" : "http://collector-"+that.valueForToken('clientID')+".tvsquared.com/piwik/");
+        	_paq.push(['setSiteId', that.valueForToken('brandID')]);
+    	    _paq.push(['setTrackerUrl', u + 'piwik.php']);
+	        _paq.push(['trackPageView']);
+        	var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]; g.type = 'text/javascript'; g.defer = true; g.async = true; g.src = u + 'piwik.js';
+    	    s.parentNode.insertBefore(g, s);
+	    })();
+	},
+	pre: function() {
+	/*PRE*/
+	/*~PRE*/
+	},
+	post: function() {
+	/*POST*/
+	/*~POST*/
+	}
+});
