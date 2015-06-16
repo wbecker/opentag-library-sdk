c2e8d1c986c7b0ca1bd7761219d7fd0c13d42ad8
Andrew Wolfram <ajwolfram@gmail.com>
2015-06-16 16:48:05 -0400

    Update Intent Media All Product Paths Tag and add Events Tag

---
+++ libraries/intentmedia/events/v1/Tag.js
@@ -0,0 +1,38 @@
+qubit.opentag.LibraryTag.define("intentmedia.events.v1.Tag", {
+  config: {
+    /*DATA*/
+    name: "Intent Media - Events",
+    description: "To be used in conjunction with the Search Compare Ads Tag to handle Intent Media events",
+    upgradeable: true;
+    html: "",
+    async: true,
+    parameters: [{
+        name: "Intent Media Event",
+        description: "Intent Media event to be triggered (e.g.,'open_exit_unit', 'fill_exit_unit', 'page_id_updated', 'onpage_ads_redraw')",
+        token: "im_event",
+        uv: "",
+        defaultValue: ""
+    }]
+	/*~DATA*/
+  },
+
+  script: function () {
+    /*SCRIPT*/
+      if(this.valueForToken("im_event")) {
+          if(IntentMedia && IntentMedia.trigger) {
+              IntentMedia.trigger(this.valueForToken("im_event"));
+          }
+      }
+  	/*~SCRIPT*/
+  },
+
+  pre: function () {
+    /*PRE*/
+  	/*~PRE*/
+  },
+
+  post: function() {
+    /*POST*/
+    /*~POST*/
+  }
+});
