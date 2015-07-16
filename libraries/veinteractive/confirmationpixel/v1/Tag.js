50ac70fe4e4ab08c5713a44f11313980a3494477
mbarceanu <Mirela.Barceanu@veinteractive.com>
2015-07-16 13:42:14 +0300

    change the tag and pixel variable VeInteractive Id
    replaced the variable from tag and pixel to take the entire script, not
    only the Id as the server can be changed

--- libraries/veinteractive/confirmationpixel/v1/Tag.js
+++ libraries/veinteractive/confirmationpixel/v1/Tag.js
@@ -6,14 +6,14 @@ qubit.opentag.LibraryTag.define("veinteractive.confirmationpixel.v1.Tag", {
 		name: "Confirmation Pixel",
 		async: true,
 		description: "Tag to be added only to the confirmation page",
-		html: "<img src=\"//drs2.veinteractive.com/DataReceiverService.asmx/Pixel?journeycode=${id}\" width=\"1\" height=\"1\"/>",
+		html: "<img src=\"${id}\" width=\"1\" height=\"1\"/>",
 		locationDetail: "",
 		isPrivate: false,
 		url: "",
 		usesDocWrite: false,
 		upgradeable: true,
 		parameters: [{
-			name: "VE Interactive ID",
+			name: "VE Interactive Pixel",
 			description: "The ID for the tag in this format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX with dashes included",
 			token: "id",
 			uv: ""
