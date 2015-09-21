6ac0f4240ad4e1b8aeaa1b4637b3a1b71cba5275
Peter Fronc <peter.fronc@qubit.com>
2015-09-21 14:28:44 +0100

    Adding wizard updates.

--- shared/template/Tag.js
+++ shared/template/Tag.js
@@ -2,7 +2,7 @@
 
 qubit.opentag.LibraryTag.define("PACKAGE_NAME.Tag", {
 	getDefaultConfig: function () {
-		/*DATA*/
+		/*config*/
 		description: "DESCRIPTION.",
 		name: "TAG_NAME",
 		async: true,
@@ -11,22 +11,22 @@ qubit.opentag.LibraryTag.define("PACKAGE_NAME.Tag", {
 		parameters: [
 			
 		]
-		/*~DATA*/
+		/*~config*/
 	},
 	script: function() {
-	/*SCRIPT*/
+	/*script*/
 		// write your code here that will be executed after all 
 		// filters, urls, html injections and dependencies pass/execute.
-	/*~SCRIPT*/
+	/*~script*/
 	},
 	pre: function() {
-	/*PRE*/
+	/*pre*/
 		//write here pre execution code
-	/*~PRE*/
+	/*~pre*/
 	},
 	post: function() {
-	/*POST*/
+	/*post*/
 		//write here post execution code
-	/*~POST*/
+	/*~post*/
 	}
 });
