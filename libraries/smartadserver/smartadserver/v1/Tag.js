ccc064b91d49316a727f3ddb6c24064c9a71766f
Kendrick <kbesnard@lefigaro.fr>
2015-09-21 16:55:57 +0200

    SmartAdServer main script #45 pull request fix

--- libraries/smartadserver/smartadserver/v1/Tag.js
+++ libraries/smartadserver/smartadserver/v1/Tag.js
@@ -1,23 +1,21 @@
 //:include tagsdk-current.js
 
 qubit.opentag.LibraryTag.define("smartadserver.smartadserver.v1.Tag", {
-	getDefaultConfig: function () {
-		return {
-			/*DATA*/
-			description: "SmartAdServer main script. To call the advertisement use into the page the sas.call() function, ex : sas.call('std', {siteId: 0000, pageId: 0000, formatId: 0000, target: ''}); and add the noscript marker. See the recommended implementation by SmartAdServer.",
-			name: "SmartAdServer main script",
-			async: true,
-			isPrivate: false,
-			html: "",
-			url: "http://r.sascdn.com/diff/js/smart.js",
-			usesDocWrite: true,
-			parameters: [{
-				name:"SmartAdServer domain",
-				description:"SmartAdServer domain : ex ww691 (ww691.smartadserver.com)",
-				token:"sas_domain",
-				uv:"universal_variable.adserver.sas_domain"
-			}]
-		};
+	config: {
+		/*DATA*/
+		description: "SmartAdServer main script. To call the advertisement use into the page the sas.call() function, ex : sas.call('std', {siteId: 0000, pageId: 0000, formatId: 0000, target: ''}); and add the noscript marker. See the recommended implementation by SmartAdServer.",
+		name: "SmartAdServer main script",
+		async: true,
+		isPrivate: false,
+		html: "",
+		url: "http://r.sascdn.com/diff/js/smart.js",
+		usesDocWrite: true,
+		parameters: [{
+			name:"SmartAdServer domain",
+			description:"SmartAdServer domain : ex ww691 (ww691.smartadserver.com)",
+			token:"sas_domain",
+			uv:"universal_variable.adserver.sas_domain"
+		}]
 		/*~DATA*/
 	},
 	script: function() {
