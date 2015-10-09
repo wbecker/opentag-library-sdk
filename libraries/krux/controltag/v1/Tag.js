3c4325b7b18348fd30aa917077a8986463067b83
Kendrick <kendrick-k@users.noreply.github.com>
2015-10-09 17:29:43 +0200

    beautify
    some..

--- libraries/krux/controltag/v1/Tag.js
+++ libraries/krux/controltag/v1/Tag.js
@@ -22,12 +22,15 @@ qubit.opentag.LibraryTag.define("krux.controltag.v1.Tag", {
 	},
 	script: function() {
 	/*SCRIPT*/
-		window.Krux||((Krux=function(){Krux.q.push(arguments)}).q=[]);
-		var k=document.createElement('script');k.type='text/javascript';k.async=true;
-		var m,src=(m=location.href.match(/\bkxsrc=([^&]+)/))&&decodeURIComponent(m[1]);
-		k.src = /^https?:\/\/([a-z0-9_\-\.]+\.)?krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" :
-		(location.protocol==="https:"?"https:":"http:")+"//cdn.krxd.net/controltag?confid=" + this.valueForToken("configuration_id");
-		var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(k,s);
+		window.Krux || (( Krux = function() { Krux.q.push(arguments) }).q=[] );
+		var k = document.createElement('script');
+		k.type = 'text/javascript';
+		k.async = true;
+		var m,src = ( m = location.href.match(/\bkxsrc=([^&]+)/)) && decodeURIComponent(m[1]);
+		k.src = /^https?:\/\/([a-z0-9_\-\.]+\.) ? krxd\.net(:\d{1,5})?\//i.test(src) ? src : src === "disable" ? "" :
+		( location.protocol === "https:" ? "https:" : "http:" ) + "//cdn.krxd.net/controltag?confid=" + this.valueForToken("configuration_id");
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(k,s);
 	/*~SCRIPT*/
 	},
 	pre: function() {
