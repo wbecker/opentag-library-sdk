33272f254e6c9ed8b4a16c18be7a35d3012e22aa
Kendrick <kendrick-k@users.noreply.github.com>
2015-10-09 17:19:50 +0200

    update
    description + unpack script

--- libraries/hotjar/hotjar/v1/Tag.js
+++ libraries/hotjar/hotjar/v1/Tag.js
@@ -5,7 +5,7 @@ qubit.opentag.LibraryTag.define("hotjar.hotjar.v1.Tag", {
 		/*DATA*/
 		name: "hotjar",
 		async: true,
-		description: "Heatmap user tracking.",
+		description: "Hotjar : all-in-one analytics and feedback. User tracking with heatmaps, record user sessions, create polls and surveys. Find the biggest opportunities for improvement and testing by identifying on which page and at which step most visitors are leaving your site.",
 		html: "",
 		locationDetail: "",
 		isPrivate: false,
@@ -13,12 +13,12 @@ qubit.opentag.LibraryTag.define("hotjar.hotjar.v1.Tag", {
 		usesDocWrite: false,
 		upgradeable: true,
 		parameters: [{
-			name: "heatmap hjid",
+			name: "hotjar hjid",
 			description: "hjid",
 			token: "hjid",
 			uv: "universal_variable.hotjar.hjid"
 		}, {
-			name: "heatmap hjsv",
+			name: "hotjar hjsv",
 			description: "hjsv",
 			token: "hjsv",
 			uv: "universal_variable.hotjar.hjsv"
@@ -30,10 +30,11 @@ qubit.opentag.LibraryTag.define("hotjar.hotjar.v1.Tag", {
 		var _this = this;
 		(function(f,b){
 			var c;
-			f.hj=f.hj||function(){(f.hj.q=f.hj.q||[]).push(arguments)};
-			f._hjSettings={hjid:_this.valueForToken('hjid'), hjsv:_this.valueForToken('hjsv')};
-			c=b.createElement("script");c.async=1;
-			c.src="//static.hotjar.com/c/hotjar-" + _this.valueForToken('hjid') + ".js?sv=" + _this.valueForToken('hjsv');
+			f.hj = f.hj || function() { ( f.hj.q = f.hj.q || [] ).push(arguments) };
+			f._hjSettings = { hjid : _this.valueForToken('hjid'), hjsv:_this.valueForToken('hjsv') };
+			c = b.createElement("script");
+			c.async = 1;
+			c.src = "//static.hotjar.com/c/hotjar-" + _this.valueForToken('hjid') + ".js?sv=" + _this.valueForToken('hjsv');
 			b.getElementsByTagName("head")[0].appendChild(c);
 		})(window,document);
 	/*~SCRIPT*/
