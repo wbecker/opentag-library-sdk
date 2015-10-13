7ca94e52a3c1e5fe4e129da6aa76590d017fdb76
Kendrick <kendrick-k@users.noreply.github.com>
2015-10-13 12:18:07 +0200

    customaudience

--- libraries/facebook/facebookconversionpixel/v1/Tag.js
+++ libraries/facebook/facebookconversionpixel/v1/Tag.js
@@ -1,9 +1,9 @@
 //:include tagsdk-current.js
 
-qubit.opentag.LibraryTag.define("facebook.facebookconversionpixel.v1.Tag", {
+qubit.opentag.LibraryTag.define("facebook.facebookcustomaudiencepixel.v1.Tag", {
 	config: {
 		/*DATA*/
-		name: "Facebook Custom Audience Pixel Code",
+		name: "Facebook Custom Audience Pixel",
 		async: true,
 		description: "The Custom Audience pixel is activated every time someone opens a webpage where the code is installed. When the pixel is activated an event is sent to Facebook's servers with general information about the browsing session including page URLs. The events are then compared to a set of Audience rules the advertiser has created. If the event matches an Audience rule the person who visited the webpage is added to that Audience. These website Custom Audiences can later be targeted by ads.",
 		html: "",
@@ -21,7 +21,7 @@ qubit.opentag.LibraryTag.define("facebook.facebookconversionpixel.v1.Tag", {
 			name: "Pixel ID",
 			description: "Client Specific (e.g. 6007143437659)",
 			token: "pixel_id",
-			uv: "universal_variable.facebook.conversion.pixel_id"
+			uv: "universal_variable.facebook.customaudience.pixel_id"
 		}]
 		/*~DATA*/
 	},
