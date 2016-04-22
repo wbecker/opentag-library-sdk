733bd6080ce425e98ab038b06311de54b81a998e
Jake Rowsell <jake@qubit.com>
2016-04-22 11:22:42 +0100

    Deliver tag added

---
+++ libraries/qubit/qubitdeliver/v1/Tag.js
@@ -0,0 +1,37 @@
+//:include tagsdk-current.js
+
+qubit.opentag.LibraryTag.define("qubit.qubitdeliver.v1.Tag", {
+	getDefaultConfig: function () {
+		return {
+			/*config*/
+		name: "Qubit - Deliver",
+		async: true,
+		description: "",
+		html: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "//dd6zx4ibq538k.cloudfront.net/smartserve-${property_id}.js",
+		usesDocWrite: false,
+		upgradeable: true,
+		parameters: [{
+            name: "Property ID",
+            description: "Client's Property ID",
+            token: "property_id",
+            uv: ""
+        }]
+			/*~config*/
+		};
+	},
+	script: function() {
+	/*script*/
+	/*~script*/
+	},
+	pre: function() {
+	/*pre*/
+	/*~pre*/
+	},
+	post: function() {
+	/*post*/
+	/*~post*/
+	}
+});
